6370ba839afe8d558d0b8f01c82f7f68f7844f47
Andras Nagy <andrasnagy097@gmail.com>
2023-01-22 20:10:19 +0100

    make db connection setup; make ORM classes

--- server/db/conn.py
+++ server/db/conn.py
@@ -1,3 +1,24 @@
 """
 Database connection setup.
 """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_CONN_STRING: str = "sqlite+pysqlite:///demo.db"
+
+engine = create_engine(DATABASE_CONN_STRING, echo=True, future=True)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, future=True
+)
+Base = declarative_base()
+
+
+def get_db():
+    """
+    DB dependency.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
