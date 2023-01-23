1f376fb4dfea522401e201ccb5715739bff807b1
Andras Nagy <andrasnagy097@gmail.com>
2023-01-23 10:04:00 +0100

    make user-followers endpoint; add logging; rename users->user

--- server/internals.py
+++ server/internals.py
@@ -1,3 +1,11 @@
 """
 Server internals.
 """
+import logging
+
+# set up logging
+logging.basicConfig(
+    filename="server.log", format="%(asctime)s %(message)s", filemode="w"
+)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
--- server/main.py
+++ server/main.py
@@ -1,3 +1,10 @@
 """
 Main entry point for application.
 """
+from fastapi import FastAPI
+
+import server.api.user as user
+
+app = FastAPI()
+
+app.include_router(user.router)
